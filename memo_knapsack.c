49d2da71b3f4a63e0636b59bf29ab9d779db3ea2
Samuel Borges <samuelmordred@gmail.com>
2018-06-04 08:45:54 -0300

    finished memoized knapsack and added time counter

--- memo_knapsack.c
+++ memo_knapsack.c
@@ -3,9 +3,15 @@
 #include<time.h>
 #define TAM 60
 #define CAP 300
+
+int memo[TAM+1][CAP+1] = {0};
+
 int max(int a, int b) { return (a > b)? a : b; }
 
 int knapsack(int capacity, int weight[], int value[], int position){
+    if(memo[position][capacity] != 0){
+        return memo[position][capacity];
+    }
     if (position == 0 || capacity == 0){
         return 0;
     }
@@ -17,6 +23,7 @@ int knapsack(int capacity, int weight[], int value[], int position){
     else {
         int lol = max( value[position-1] + knapsack(capacity - weight[position-1], weight, value, position-1),
         knapsack(capacity, weight, value, position-1));
+        memo[position][capacity] = lol;
         return lol;
     }
 }
@@ -38,7 +45,7 @@ int main(){
     clock_t begin = clock();
 
     int result = knapsack(capacity, weight, value, position);
-    
+
     clock_t end = clock();
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 
